586a279ea06b484ef31125030224c2ebdcf55986
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2022-02-11 00:09:14 +0500

    add

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -2,12 +2,16 @@ import "./home.css";
 
 import Header from "../../Components/header/header";
 import Intro from "../../Components/intro/intro";
+import Sura from "../../Components/sura/sura";
+import JeromeBell from "../../Components/JeromeBell/JeromeBell";
 
 function Home() {
   return (
     <div>
       <Header />
       <Intro />
+      <Sura />
+      <JeromeBell />
     </div>
   );
 }
