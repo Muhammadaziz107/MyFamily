d5ec2bbe9412cac22d9ac6937526d418fe034e46
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2022-02-13 13:55:06 +0500

    add

---
+++ src/Components/teacher/teachers.jsx
@@ -0,0 +1,10 @@
+import "./teachers.css";
+
+function Teachers() {
+  return (
+    <div>
+      <div className="container">teachers</div>
+    </div>
+  );
+}
+export default Teachers;
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,19 +1,32 @@
 import "./home.css";
 
+//components
 import Header from "../../Components/header/header";
 import Intro from "../../Components/intro/intro";
 import Sura from "../../Components/sura/sura";
 import JeromeBell from "../../Components/JeromeBell/JeromeBell";
 import Footer from "../../Components/footer/footer";
+import WhyChoise from "../../Components/whyChoise/whyChoise";
+import Teachers from "../../Components/teacher/teachers";
 
 function Home() {
   return (
     <div>
-      <Header />
-      <Intro />
-      <Sura />
-      <JeromeBell />
-      <Footer />
+      <header>
+        <Header />
+      </header>
+
+      <section>
+        <Intro />
+        <Sura />
+        <WhyChoise />
+        <Teachers />
+        <JeromeBell />
+      </section>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
