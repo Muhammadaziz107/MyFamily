3026632799f3cb0718b808345cbe635d3e1e0cdd
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2022-02-13 16:02:46 +0500

    finish home page

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,46 +1,47 @@
 // import { useState } from 'react';
-import './Courses.css'
+import "./Courses.css";
 // import pexels from '../../Assets/Images/pexels-monstera-5997151.png'
-import {courses} from '../data'
-import { Link } from 'react-router-dom'
+import { courses } from "../data";
+import { Link } from "react-router-dom";
 
-let s = courses.slice(0, 4)
-s.reverse()
-
+let s = courses.slice(0, 4);
+s.reverse();
 
 function Courses() {
+  return (
+    <>
+      <section className="courses">
+        <div className="container">
+          <h2 className="courses__title">Bizning kurslar</h2>
+          <p className="courses__subtitle">
+            Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+            laudantium
+          </p>
 
-
-
-    return (<>
-        <section className="courses">
-            <div className="container">
-                <h2 className="courses__title">Bizning kurslar</h2>
-                <p className="courses__subtitle">Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
-
-                <ul className="card-wrapper">
-                    {s.map((e) => {
-                        return (
-                            <li key={e.id} className="card">
-                                <div className="img-wrapper">
-                                    <span className="card__btn">{e.author}</span>
-                                    <img src={e.image} alt="e" className="card__img" />
-                                </div>
-                                <h3 className="card__title">{e.title}</h3>
-                                <p className="card__description">{e.subtitle}</p>
-                                <a href="/" className="card__link">Batafsil</a>
-                            </li>
-                        )
-                    })}
-
-                </ul>
-                <Link to="" className="courses__btn">Barcha kurslar</Link>
-            </div>
-        </section>
-      
-    </>)
+          <ul className="card-wrapper">
+            {s.map(e => {
+              return (
+                <li key={e.id} className="card">
+                  <div className="img-wrapper">
+                    <span className="card__btn">{e.author}</span>
+                    <img src={e.image} alt="e" className="card__img" />
+                  </div>
+                  <h3 className="card__title">{e.title}</h3>
+                  <p className="card__description">{e.subtitle}</p>
+                  <a href="/" className="card__link">
+                    Batafsil
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+          <Link to="" className="courses__btn">
+            Barcha kurslar
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 }
-
-
 
 export default Courses;
