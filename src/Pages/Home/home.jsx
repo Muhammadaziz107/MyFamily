02c151f71e8274d83abcfa4d587769cf4fe3d48f
Muzaffarxon04 <muzaffarxonabdusalomov04@gmail.com>
2022-02-15 18:39:26 +0500

    changes container style mediq qery

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -6,29 +6,16 @@ import Intro from "../../Components/intro/intro";
 import Sura from "../../Components/sura/sura";
 import JeromeBell from "../../Components/JeromeBell/JeromeBell";
 import Footer from "../../Components/footer/footer";
-<<<<<<< HEAD
-import Courses from '../../Components/Courses/Courses'
-import Blogs from '../../Components/Blogs/Blogs'
-
-=======
 import WhyChoise from "../../Components/whyChoise/whyChoise";
 import Courses from "../../Components/Courses/Courses";
 import Blogs from "../../Components/Blogs/Blogs";
 import Teachers from "../../Components/teacher/teachers";
->>>>>>> fd188e10102537528242f73ca3359841b738b110
+
 
 function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <Header />
-      <Intro />
-      <Sura />
-<Courses />
-      <Blogs />
-      <JeromeBell />
-      <Footer />
-=======
+
       <header>
         <Header />
       </header>
@@ -45,7 +32,6 @@ function Home() {
       <footer>
         <Footer />
       </footer>
->>>>>>> fd188e10102537528242f73ca3359841b738b110
     </div>
   );
 }
