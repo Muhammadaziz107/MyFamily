8e25f9d836e28a86576a77211fc1b04ae7314b33
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2022-02-16 21:36:58 +0500

    add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./App.css";
 import Home from "./Pages/Home/home";
+import Contact from "./Pages/Contact/contact";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
