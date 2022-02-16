dda33bdefce935a0f26f6b5e8661f64ada514e65
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2022-02-16 21:18:27 +0500

    finish home page

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -6,47 +6,34 @@ import Intro from "../../Components/intro/intro";
 import Sura from "../../Components/sura/sura";
 import JeromeBell from "../../Components/JeromeBell/JeromeBell";
 import Footer from "../../Components/footer/footer";
-<<<<<<< HEAD
-import Courses from '../../Components/Courses/Courses'
-import Blogs from '../../Components/Blogs/Blogs'
-
-=======
-import WhyChoise from "../../Components/whyChoise/whyChoise";
 import Courses from "../../Components/Courses/Courses";
 import Blogs from "../../Components/Blogs/Blogs";
+
+import WhyChoise from "../../Components/whyChoise/whyChoise";
 import Teachers from "../../Components/teacher/teachers";
->>>>>>> fd188e10102537528242f73ca3359841b738b110
 
 function Home() {
   return (
-    <div>
-<<<<<<< HEAD
-      <Header />
-      <Intro />
-      <Sura />
-<Courses />
-      <Blogs />
-      <JeromeBell />
-      <Footer />
-=======
-      <header>
-        <Header />
-      </header>
-      <section>
-        <Intro />
-        <Sura />
-        <WhyChoise />
-        {/* <Courses /> */}
-        {/* <Blogs /> */}
-        {/* <Teachers /> */}
-        <JeromeBell />
-      </section>
+    <>
+      <div>
+        <header>
+          <Header />
+        </header>
+        <section>
+          <Intro />
+          <Sura />
+          <WhyChoise />
+          <Courses />
+          <Blogs />
+          {/* <Teachers /> */}
+          <JeromeBell />
+        </section>
 
-      <footer>
-        <Footer />
-      </footer>
->>>>>>> fd188e10102537528242f73ca3359841b738b110
-    </div>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
 }
 
